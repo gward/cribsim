3f67d534f3f4326b2ade9a970511cceeb0732403
Greg Ward <greg@gerg.ca>
2024-04-23 15:49:03 -0400

    be a little smarter about seeding rand()

--- c/cribsim.c
+++ c/cribsim.c
@@ -1,13 +1,17 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
+#include <sys/types.h>
 
 #include "cards.h"
 #include "play.h"
 
 int main(int argc, char *argv[]) {
-    uint seed = (uint) getpid();
-    seed ^= (uint) time(NULL);
+    time_t now = time(NULL);
+    pid_t pid = getpid();
+    uint seed = (int) now ^ (int) pid ^ ((int) pid << 16);
+    printf("now = %ld, pid = %d, seed = %ud\n", now, pid, seed);
     srand(seed);
 
     deck_t *deck = new_deck();
